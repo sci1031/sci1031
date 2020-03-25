1a344e94e47bd93af750484f745f0a5dafe5e11d
Elise Filotas <elise.filotas@gmail.com>
2020-03-25 18:50:38 -0400

    petits changements

--- root_path.R
+++ root_path.R
@@ -1,4 +1,7 @@
 root_path<-function(){
+  #laptop
+  #root = "C:/Users/Elise/Dropbox/TELUQ/ENSEIGNEMENT/Cours/AnalyseSpatiale/Developpement"
+  
   #Maison
   root = "E:/ELF/Dropbox/TELUQ/ENSEIGNEMENT/Cours/AnalyseSpatiale/Developpement"
   
